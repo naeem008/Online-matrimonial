6c2f3651196bc5be43b0bfef971d0e74132974fa
naeem008 <zannatnaeem3@gmail.com>
2017-12-17 17:29:39 +0600

    ajax work,database input

--- naeem1/ajax_action/basic_info_lifestyle_update.php
+++ naeem1/ajax_action/basic_info_lifestyle_update.php
@@ -6,13 +6,18 @@ $about=$_POST['about'];
 $marital_status=$_POST['marital_status'];
 $diet=$_POST['diet'];
 $height=$_POST['height'];
+$skin=$_POST['skin'];
+$haircolor=$_POST['hair'];
+$eyecolor=$_POST['eye'];
+$freckles=$_POST['freckle'];
+$dimple=$_POST['dimple'];
 $weight=$_POST['weight'];
 $habits=$_POST['habits'];
 $mother_tongue=$_POST['mother_tongue'];
 $languages=$_POST['languages'];
 $blood=$_POST['blood'];
 $disability=$_POST['disability'];
-$basic_sql="UPDATE basic_info SET about='$about', marital_status='$marital_status', diet='$diet', height='$height' WHERE email='$email'";
+$basic_sql="UPDATE basic_info SET about='$about', marital_status='$marital_status', diet='$diet', height='$height', skintone='$skin', haircolor='$haircolor', eyecolor='$eyecolor', dimple='$dimple', freckles='$freckles' WHERE email='$email'";
 if (mysqli_query($con,$basic_sql)) {
 	$lifestyle_sql="UPDATE lifestyle SET weight='$weight', habits='$habits', mother_tongue='$mother_tongue', language='$languages', blood='$blood', disability='$disability' WHERE email='$email'";
 	if (mysqli_query($con,$lifestyle_sql)) {
--- naeem1/connection.php
+++ naeem1/connection.php
@@ -1,5 +1,5 @@
 <?php 
-$con=new mysqli('127.0.0.1','root','','naeem');
+$con=new mysqli('127.0.0.1','root','','naeem1');
 if (!$con) {
 	echo "database is not connected";
 }
